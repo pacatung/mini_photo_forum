d2a14eec56eb08f20a3e2d1ab097eb9ceb553a09
BigBoss <pacatung@gmail.com>
2015-04-21 16:39:18 +0800

    photo/index fixed, log in successful

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
 
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+
   helper_method :current_user
 
   protected
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,6 +12,9 @@ Devise.setup do |config|
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
+  # add facebook key to devise
+  config.omniauth :facebook, "1437785176515956", "55c6d78b7e70fffd9bba4cb6ac616802"
+
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
